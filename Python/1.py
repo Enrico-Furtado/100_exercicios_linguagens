96651858b55cca717661910798543f218d8055d1
Enrico-Furtado <enricofurtado.econ@gmail.com>
2024-11-27 21:19:29 -0300

    primeiro exercício

--- Python/1.py
+++ Python/1.py
@@ -1,1 +1,4 @@
 # Exercício 1 - Crie um script de python que leia o nome da pessoa e mostre uma mensagem de boas vindas de acordo com o valor digitado.
+
+nome = str(input("Digite o seu nome: "))
+print(f"Seja bem-vindo(a) {nome}!")
