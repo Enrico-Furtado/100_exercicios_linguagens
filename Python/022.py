777894ce50f476b39c81242daaa186805eea4fb1
Enrico-Furtado <enricofurtado.econ@gmail.com>
2024-12-25 22:05:30 -0300

    Update 022.py

--- Python/022.py
+++ Python/022.py
@@ -1,3 +1,9 @@
 '''Crie um programa que leia o nome completo de uma pessoa e mostre: 1 - O nome com todas as letras maiúsculas. 
 2 - O nome com todas minúsculas. 3 - Quantas letras ao todo (sem considerar espaços). 4 - Quantas letras tem o primeiro nome.'''
 
+nome = str(input('Digite o seu nome completo: '))
+nsplit = nome.split()
+print(nome.upper())
+print(nome.lower())
+print(len(nome)-nome.count(' '))
+print(nsplit[1])
