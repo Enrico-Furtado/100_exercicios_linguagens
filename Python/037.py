63f114458949b9dd3ff088b59864a3117e151917
Enrico-Furtado <enricofurtado.econ@gmail.com>
2025-01-18 21:07:45 -0300

    ex_38

--- Python/037.py
+++ Python/037.py
@@ -1,2 +1,14 @@
 '''37 - Escreva um programa que leia um número inteiro qualquer e peça para o usuário escolher qual será a base de conversão.
 1- Para Binário. 2 - Para Octal. 3 - Para hexadecimal.'''
+
+num = int(input("Digite um número inteiro qualquer: "))
+base = int(input("Agora digite a base de conversão: 1 - Binário , 2 - Octal. 3 - Hexadecimal."))
+bina = bin(num)
+octa = oct(num)
+hexa = hex(num)
+if base == 1:
+    print(f"O número convertido para binário é {bina}")
+elif base == 2:
+    print(f"O número convertido para binário é {octa}")
+elif base == 3:
+    print(f"O número convertido para binário é {hexa}")
