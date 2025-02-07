c8ff1101129d80b065d56d5f69e08901a94682fc
Enrico-Furtado <enricofurtado.econ@gmail.com>
2025-02-07 14:55:38 -0300

    ex_69_70

--- Python/069.py
+++ Python/069.py
@@ -2,3 +2,21 @@
 se o usuário quer ou não continuar. No final mostra: A) quantas pessoas tem mais de 18 anos . B) quantos homens foram cadastrados. 
 C) quantas mulheres possuem menos de 20 anos.'''
 
+m_vinte, h, m_dezoito = 0, 0, 0
+
+while True:
+    sex = str(input("Digite o sexo da pessoa [M|F]: ")).lower()
+    idade = int(input("Digite a idade da pessoa: "))
+    if idade > 18:
+        m_dezoito += 1
+    if sex == 'f' and idade < 20:
+        m_vinte += 1
+    elif sex == 'm':
+        h += 1
+    conti = str(input("Você gostaria de continuar cadastrando pessoas? [S|N]\n")).lower()
+    if conti == 's':
+        print("Vamos continuar a cadastrar!")
+    else:
+        break
+
+print(f"Dos cadastrados, {m_dezoito} possuem mais de 18 anos,\n {h} são homens e {m_vinte} são mulheres abaixo dos 20 anos.")
--- Python/070.py
+++ Python/070.py
@@ -1,2 +1,28 @@
 '''70 - Crie um programa que leia o nome e o preço de vários produtos. O programa deverá perguntar se o usuário vai continuar. 
 No final mostre: A) Qual o gasto total da compra. B) Quantos produtos custam mais de R$1000. C) Qual o nome do produto mais barato.'''
+
+soma, p_mil, menor = 0, 0, 0
+nome_menor = ""
+cont = 0
+while True:
+    nome = str(input("Digite o nome do produto: "))
+    preco = float(input("Digite o preço do produto: "))
+    soma += preco
+    cont += 1
+    if preco > 1000:
+        p_mil += 1
+    if cont == 1:
+        menor = preco
+        nome_menor = nome
+    else:
+        if preco < menor:
+            menor = preco
+            nome_menor = nome
+    conti = str(input('Gostaria de continuar cadastrando produtos? [S|N]')).lower()
+    if conti == 's':
+        print("Vamos cadastrar mais um produto!")
+    else:
+        break
+print(f'O gasto total da compra foi de R${soma:.2f}.')
+print(f'No total, {p_mil} produtos custaram mais de R$1000,00.')
+print(f'O produto mais barato foi {nome_menor}')
