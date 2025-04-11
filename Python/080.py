d0d7b9e68674bd40b9653d5f1695625c5cec785b
Enrico-Furtado <enricofurtado.econ@gmail.com>
2025-04-11 13:54:22 -0300

    ex_80

--- Python/080.py
+++ Python/080.py
@@ -1,2 +1,34 @@
-'''80 - Crie um programa onde o usuário possa digitar cinco valores numnéricos e cadastre-os em uma lista, 
+'''80 - Crie um programa onde o usuário possa digitar cinco valores numéricos e cadastre-os em uma lista, 
 já na posição correta de inserção (sem usar o sort()). No final, mostre a lista ordenada na tela.'''
+
+lista = []
+cont = 0
+maior  = 0
+menor = 0
+
+while cont < 5:
+    valor = int(input("Digite um número inteiro e vamos cadastrá-lo em uma lista: "))
+    if len(lista) == 0:
+        lista.append(valor)
+        maior = valor
+        menor = valor
+    elif valor > maior:
+        maior = valor
+        lista.append(valor)
+    elif valor < menor:
+        menor = valor
+        lista.insert(0, valor)
+    elif valor in lista:
+        lista.insert(lista.index[valor], valor)
+    else:
+        for i in lista:
+            if valor < lista[i]:
+                lista.insert(i, valor)
+                break
+            elif valor > lista[i]:
+                lista.insert(i + 1, valor)
+                break
+    cont += 1
+
+print(lista)
+
